0ddb6f50d23cf736be4e35f1c669a45ae02d4aea
anton <antonkuzmn1@gmail.com>
2024-09-29 14:51:56 +0700

    lab 3 done

--- app/lab/lab_3/main.py
+++ app/lab/lab_3/main.py
@@ -1,2 +1,14 @@
+def split_string(input_str, char):
+    pos = input_str.find(char)
+    if pos == -1:
+        return 0
+    return input_str[:pos], input_str[pos + 1:], pos
+
+
 def main():
     print('lab3')
+
+    input_str = "hello, world"
+    char = ","
+    result = split_string(input_str, char)
+    print(result)
