7459f64622051206dcaabe4119ce1c2dcec0163f
anton <antonkuzmn1@gmail.com>
2024-10-08 10:19:22 +0700

    lab2 done

--- app/lab/lab_2/main.py
+++ app/lab/lab_2/main.py
@@ -1,37 +1,72 @@
 import random
-from typing import Any
-
-import numpy as np
-from numpy import bool, unsignedinteger, signedinteger, ndarray, dtype
-from numpy._typing import _8Bit, _16Bit, _32Bit, _64Bit
-
-
-def get_matrix(length_x: int, length_y: int) -> int | bool | bool | unsignedinteger[_8Bit] | unsignedinteger[_16Bit] | \
-                                                unsignedinteger[_32Bit] | unsignedinteger[Any] | unsignedinteger[
-                                                    _64Bit] | signedinteger[_8Bit] | signedinteger[_16Bit] | \
-                                                signedinteger[_32Bit] | signedinteger[Any] | signedinteger[_64Bit] | \
-                                                ndarray[Any, dtype[signedinteger[Any]]] | ndarray[Any, dtype[bool]] | \
-                                                ndarray[Any, dtype[signedinteger[_8Bit]]] | ndarray[
-                                                    Any, dtype[signedinteger[_16Bit]]] | ndarray[
-                                                    Any, dtype[signedinteger[_32Bit]]] | ndarray[
-                                                    Any, dtype[signedinteger[_64Bit]]] | ndarray[
-                                                    Any, dtype[unsignedinteger[_8Bit]]] | ndarray[
-                                                    Any, dtype[unsignedinteger[_16Bit]]] | ndarray[
-                                                    Any, dtype[unsignedinteger[_32Bit]]] | ndarray[
-                                                    Any, dtype[unsignedinteger[_64Bit]]] | ndarray[
-                                                    Any, dtype[unsignedinteger[Any]]]:
-    range_min = -10
-    range_max = 5
-
-    return np.random.randint(range_min, range_max, (length_x, length_y))
-
 
 def main():
     # noinspection SpellCheckingInspection
     print('2.5 Обработка матриц')
 
-    matrix = get_matrix(5, 5)
-    print(matrix)
+    size = 8
+    mat = [[random.randint(1, 10) for i in range(size)] for j in range(size)]
+
+    for row in mat:
+        for x in row:
+            print(f"{x:4d}", end="")
+        print()
+
+    main_diag = [mat[i][i] for i in range(size)]
+    side_diag = [mat[i][size - 1 - i] for i in range(size)]
+    p = sum(main_diag) - sum(side_diag)
+    print("Indicator is equal to: " + str(p))
+
+    transposed_mat = list(zip(*mat))
+    p = p % size
+    shifted_mat = transposed_mat[-p:] + transposed_mat[:-p]
+    mat = list(zip(*shifted_mat))  # транспонируем обратно
+    print(f"New matrix is shifted {p % size} columns right")
+
+    for row in mat:
+        for x in row:
+            print(f"{x:4d}", end="")
+        print()
+    print()
+
+    x = y = 9
+    m = [[0 for j in range(y)] for i in range(x)]  #
+    с = 0
+
+    for i in range(0, 9):
+        ii = 0
+        jj = i
+        for j in range(0, i + 1):
+            с += 1
+            m[ii][jj] = с
+            ii += 1
+            jj -= 1
+
+    for i in range(1, 9):
+        ii = i
+        jj = 8
+        for j in range(1, 9 - i + 1):
+            с += 1
+            m[ii][jj] = с
+            ii += 1
+            jj -= 1
+    print("Matrix from an illustration is: ")
+
+    for row in m:
+        for x in row:
+            print(f"{x:4d}", end="")
+        print()
+    print()
+
+    for i in range(9):
+        for j in range(9 - i):
+            m[i][j], m[8 - j][8 - i] = m[8 - j][8 - i], m[i][j]
+    print("Reflected matrix is: ")
+
+    for row in m:
+        for x in row:
+            print(f"{x:4d}", end="")
+        print()
 
 if __name__ == '__main__':
     main()
